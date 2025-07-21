1bae25ec3426b06b8bbc742f75b069149206b4be
Malik G.J. Karunanayake <giv.joseph2002@gmail.com>
2025-07-20 20:53:55 -0500

    Updated Customer Creation UI, and Delivery & Driver Screens UI

--- app/src/main/java/com/example/linza_apps/ui/screens/DeliveryScreen.kt
+++ app/src/main/java/com/example/linza_apps/ui/screens/DeliveryScreen.kt
@@ -4,15 +4,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import com.example.linza_apps.R
 import com.example.linza_apps.ui.components.*
@@ -49,8 +55,93 @@ fun DeliveryContent(modifier: Modifier = Modifier) {
         )
         Column {
             DateTimeDisplay()
+            Deliveries()
         }
     }
 
+}
+
+@Preview
+@Composable
+fun Deliveries() {
+    Box(Modifier.fillMaxSize()) {
+        Column() {
+            Row(Modifier.fillMaxWidth().weight(6f)) {
+                //Order ID Column
+                Box(Modifier.background(Color.White).fillMaxHeight().weight(2f)) {
+                    Column {
+                        Box(Modifier.background(Color.Gray).fillMaxWidth()) {
+                            Text("Order ID",
+                                modifier = Modifier.align(Alignment.Center), color = Color.Black)
+                        }
+                        Box() {
+                            Column() { //Column for test text
+                                Text("Test Filler Text", color = Color.Black)
+                                Text("1.", color = Color.Black)
+                                Text("2.", color = Color.Black)
+                                Text("3.", color = Color.Black)
+                            }
+                        }
+                    }
+                }
+
+                //Address Column
+                Box(Modifier.background(Color.Gray).fillMaxHeight().weight(8f)) {
+                    Column {
+                        Box(Modifier.background(Color.White).fillMaxWidth()) {
+                            Text("Address",
+                                modifier = Modifier.align(Alignment.Center), color = Color.Black)
+                        }
+                        Box() {
+                            Column() { //Column for test text
+                                Text("Test Filler Text", color = Color.Black)
+                                Text("144/4 Wewalduwa Rd, Kelaniya", color = Color.Black)
+                                Text("Wattala", color = Color.Black)
+                                Text("Dalugama", color = Color.Black)
+                            }
+                        }
+                    }
+                }
+
+                //Out for Delivery Column
+                Box(Modifier.background(Color.White).fillMaxHeight().weight(3f)) {
+                    Column {
+                        Box(Modifier.background(Color.Gray).fillMaxWidth()) {
+                            Text(text = "Out for Delivery",
+                                modifier = Modifier.align(Alignment.Center), color = Color.Black)
+                        }
+                        Box() {
+                            Column() { //Column for test text
+                                Text("Test Filler Text", color = Color.Black)
+                                Text("Driver 1", color = Color.Black)
+                                Text("Driver 4", color = Color.Black)
+                            }
+                        }
+                    }
+                }
+
+                //Available Drivers Column
+                Box(Modifier.background(Color.Gray).fillMaxHeight().weight(3f)) {
+                    Column {
+                        Box(Modifier.background(Color.White).fillMaxWidth()) {
+                            Text("Available Drivers",
+                                modifier = Modifier.align(Alignment.Center), color = Color.Black)
+                        }
+                        Box() {
+                            Column() { //Column for test text
+                                Text("Test Filler Text", color = Color.Black)
+                                Text("Driver 2", color = Color.Black)
+                                Text("Driver 3", color = Color.Black)
+                            }
+                        }
+                    }
+                }
+            }
+            Box(Modifier.fillMaxWidth().background(Color.DarkGray).weight(1f)) {
+                Text("Map buttons etc",
+                    modifier = Modifier.align(Alignment.Center))
+            }
+        }
+    }
 }
 
